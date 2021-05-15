933bbcce925945fc049f687f4efe72c2442b391a
richard jarram <rjarram@me.com>
2021-05-14 21:43:41 -0700

    built

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
+import React, { useState } from 'react'
+import _ from 'lodash'
 import logo from './logo.svg';
+import eightball from './eightball.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={eightball} className="App-logo" alt="logo" />
+        <EightBall/>
       </header>
     </div>
   );
+}
+
+function EightBall() {
+  const [thinking, setThinking] = useState(false)
+  const [profession, setProfession] = useState(false)
+
+  const professions = [
+    'Lion Tamer',
+    'Dentist',
+    'Magician',
+    'Escort',
+    'Programmer',
+    'Musician',
+    'President'
+  ]
+
+  const handleQuestion = () => {
+    setThinking(true)
+    setTimeout(() => { 
+      const randomIndex = _.random(0, professions.length-1)
+      setProfession(professions[randomIndex])
+      setThinking(false) 
+    }, 2000)
+  }
+
+  return (
+    <>
+      {
+        !profession && (
+          <p>
+            Lara, what profession will you be?
+          </p>
+        )
+      }
+      {
+        profession && (
+          <p>
+            I think you should be a {profession}!
+          </p>
+        )
+      }
+      {
+        thinking && (
+          <button
+            disabled
+          >
+            Hmmm... give me a second or two, I'm thinking...
+          </button>
+        )
+      }
+      {
+        !thinking && !profession && (
+          <button
+            onClick={handleQuestion}
+          >
+            Ask me!
+          </button>
+        )
+      }
+      {
+        !thinking && profession && (
+          <button
+           onClick={handleQuestion}
+          >
+            Ask me again!
+          </button>
+        )
+      }
+    </>
+  )
 }
 
 export default App;
